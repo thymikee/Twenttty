85e59444f6a3f81330c6dcdc89f58687d9c0e8e1
Michał Pierzchała <thymikee@gmail.com>
2019-12-25 22:53:41 +0100

    init

---
+++ twenty/twenty/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  twenty
+//
+//  Created by Michał Pierzchała on 25/12/2019.
+//  Copyright © 2019 Michał Pierzchała. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ twenty/twenty/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  twenty
+//
+//  Created by Michał Pierzchała on 25/12/2019.
+//  Copyright © 2019 Michał Pierzchała. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
