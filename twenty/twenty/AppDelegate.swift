fc66e9e0b1f11b6052ae6376bb1634f93f478f5a
Michał Pierzchała <thymikee@gmail.com>
2019-12-29 17:30:49 +0100

    some base

--- twenty/twenty/AppDelegate.swift
+++ twenty/twenty/AppDelegate.swift
@@ -10,17 +10,41 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        statusItem.button?.title = "👀"
+        statusItem.button?.target = self
+        statusItem.button?.action = #selector(showSettings)
+        
+//        // Check if the launcher app is started
+//        var startedAtLogin = false
+//        for app in NSWorkspace.shared().runningApplications {
+//            if app.bundleIdentifier == NCConstants.launcherApplicationIdentifier {
+//                startedAtLogin = true
+//            }
+//        }
+//
+//        // If the app's started, post to the notification center to kill the launcher app
+//        if startedAtLogin {
+//            DistributedNotificationCenter.default().postNotificationName(NCConstants.KILLME, object: Bundle.main.bundleIdentifier, userInfo: nil, options: DistributedNotificationCenter.Options.deliverImmediately)
+//        }
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
-
+    @objc func showSettings() {
+        let storyboard = NSStoryboard(name: "Main", bundle: nil)
+        guard let vc = storyboard.instantiateController(withIdentifier: "ViewController") as? ViewController else {
+            fatalError("Unable to find ViewController")
+        }
+        
+        let popoverView = NSPopover()
+        popoverView.contentViewController = vc
+        popoverView.behavior = .transient
+        popoverView.show(relativeTo: statusItem.button!.bounds, of: statusItem.button!, preferredEdge: .maxY )
+    }
 }
 
