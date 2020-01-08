644cd0dd988ed15a4662a90d17ce4be9493fdd96
Michał Pierzchała <thymikee@gmail.com>
2020-01-08 23:35:22 +0100

    --amend

--- twenty/twenty/AppDelegate.swift
+++ twenty/twenty/AppDelegate.swift
@@ -14,34 +14,38 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        let icon = NSImage(named: "0")
+        let icon = NSImage(named: "12")
         icon?.isTemplate = true
         statusItem.button?.image = icon
         statusItem.button?.target = self
         statusItem.button?.action = #selector(showSettings)
-        
+
         Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { _ in
-            if (self.activityStatus.activityTimer.isValid) {
-                let timeLeft = abs(Date().timeIntervalSince(self.activityStatus.activityTimer.fireDate))
-                let iconName = self.getImageForTimeLeft(timeLeft, self.activityStatus.activityTime)
-                let icon = NSImage(named: iconName)
-                icon?.isTemplate = true
-                self.statusItem.button?.image = icon
-                self.statusItem.button?.contentTintColor = nil
-            } else if (self.activityStatus.breakTimer.isValid) {
-                let timeLeft = abs(Date().timeIntervalSince(self.activityStatus.breakTimer.fireDate))
-                let iconName = self.getImageForTimeLeft(timeLeft, self.activityStatus.breakTime)
-                let icon = NSImage(named: iconName)
-                icon?.isTemplate = true
-                self.statusItem.button?.image = icon
-                self.statusItem.button?.contentTintColor = NSColor.red
-            } else {
-                self.statusItem.button?.image = icon
-                self.statusItem.button?.contentTintColor = nil
-            }
+            self.updateStatusItemImage(icon)
         })
     }
-    
+
+    func updateStatusItemImage(_ defaultIcon: NSImage?) {
+        if (activityStatus.activityTimer.isValid) {
+            let timeLeft = abs(Date().timeIntervalSince(activityStatus.activityTimer.fireDate))
+            let iconName = getImageForTimeLeft(timeLeft, activityStatus.activityTime)
+            let icon = NSImage(named: iconName)
+            icon?.isTemplate = true
+            statusItem.button?.image = icon
+            statusItem.button?.contentTintColor = nil
+        } else if (activityStatus.breakTimer.isValid) {
+            let timeLeft = abs(Date().timeIntervalSince(activityStatus.breakTimer.fireDate))
+            let iconName = getImageForTimeLeft(timeLeft, activityStatus.breakTime)
+            let icon = NSImage(named: iconName)
+            icon?.isTemplate = true
+            statusItem.button?.image = icon
+            statusItem.button?.contentTintColor = NSColor.red
+        } else {
+            statusItem.button?.image = defaultIcon
+            statusItem.button?.contentTintColor = nil
+        }
+    }
+
     func getImageForTimeLeft(_ timeLeft: TimeInterval, _ base: Double) -> String {
         let timePercent = timeLeft / base
         return String(Int(floor((1 - timePercent) * 12)))
@@ -56,9 +60,9 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         guard let vc = storyboard.instantiateController(withIdentifier: "ViewController") as? ViewController else {
             fatalError("Unable to find ViewController")
         }
-        
+
         vc.activityStatus = activityStatus
-        
+
         let popoverView = NSPopover()
         popoverView.contentViewController = vc
         popoverView.behavior = .transient
